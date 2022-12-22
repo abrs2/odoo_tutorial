38836d25450167838ede71d73af33ab5e78681c0
abrs2 <abarrios@pao-mx.com>
2022-12-22 12:07:36 -0700

    Fifth  commit

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -4,13 +4,23 @@
         'base'
     ], 
     'data':[
+
+        #security
         'security/ir.model.access.csv',
+        #data
+        'views/dashboard_action.xml',
+        #demo
+        #reports
+        #views
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/res_users_properties_views.xml',
         'views/estate_menus.xml',
+    ],
+    'qweb':[
+        'static/src/dashboard.xml',
     ],
     'application':'True',                            
 }
