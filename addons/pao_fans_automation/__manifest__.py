ea2ba420a48c1f532a94fb81bb746b373b9077d1
abrs2 <abarrios@pao-mx.com>
2023-04-25 19:06:44 -0600

    Nineth Commit

---
+++ addons/pao_fans_automation/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'PAO Fans Automation',
+    'version':'1.0',
+    'author':'Abrahan Barrios',
+    'category':'',
+    'website':'https://paomx.com',
+    'depends':['sale','base','portal'
+    ],
+    'data':[
+        #security
+        'security/ir.model.access.csv',
+        #data
+        #demo
+        #reports
+        #views
+        'views/aplications_menus.xml',
+        'views/global_gap_application_views.xml'
+
+    ],
+    
+}
---
+++ addons/pao_fans_automation/models/GlobalGapApplication.py
@@ -0,0 +1,16 @@
+from odoo import fields,models,api
+
+class GlobalGapApplication(models.Model):
+    
+    _name = "pao.global.gap.application"
+    _description = "PAO Global Gap Application"
+    _rec_name = 'plNumber'
+
+    plNumber = fields.Char("PL-México",required=True,copy=False,help="Asigned by Primus Auditing Operations")
+    ggn = fields.Char(string="GGN",required=True,help="Asigned by GLOBAL G.A.P.")
+    version = fields.Selection(
+        string='Version',
+        selection=[('5.4-1','V5.4-1-GFS'), ('5.3','V5.3-GFS')],
+        help="It refers to the Global Gap version on which the customer would like to get certified", default= '5.4-1',
+        )
+    
