419cb32b58dabe04a0d1b0c8867f0eb5b3dd53b4
abrs2 <abarrios@pao-mx.com>
2023-09-13 12:35:13 -0700

    test

--- addons/custom/controllers/web_form.py
+++ addons/custom/controllers/web_form.py
@@ -1,15 +1,25 @@
 from odoo import http
-from odoo.http import request
+from odoo.http import request,json
 import logging
 
 _logger = logging.getLogger(__name__)
 
 class WebForm(http.Controller):
-
-	@http.route('/submit_web_form', type='http', website=True, auth="public")
-	def submit_form(self, **kw):
-    		return request.render('custom.form_template', {})
-
+    
+    @http.route('/submit_web_form', type='http', website=True, auth="public")
+    def submit_form(self, **kw):
+         return request.render('custom.form_template', {})
+    
+    @http.route('/create/web_form_record', type='http', website=True, auth="public", Methods=['POST'])
+    def create_form(self, **kw):
+        data = json.loads(kw['data_line_ids'])
+        val = [(0, 0, line) for line in data]
+        values = {
+            'name': kw['name'],
+            'data_line_ids': val,
+        }
+        form_id = request.env['web.form.test'].sudo().create(values)
+        return request.render('web_form.form_template', {})
 
 
 
