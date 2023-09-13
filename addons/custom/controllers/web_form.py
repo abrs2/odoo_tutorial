3de44876df6f40a9f08bf7af29a25e8ca6b3fa4f
abrs2 <abarrios@pao-mx.com>
2023-09-13 12:09:42 -0700

    test

--- addons/custom/controllers/web_form.py
+++ addons/custom/controllers/web_form.py
@@ -8,7 +8,7 @@ class WebForm(http.Controller):
 
 	@http.route('/submit_web_form', type='http', website=True, auth="public")
 	def submit_form(self, **kw):
-    		return request.render('web_form.form_template', {})
+    		return request.render('custom.form_template', {})
 
 
 
